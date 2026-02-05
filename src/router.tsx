bd58e286e384e539b4e607ef45eea8b2e483c64c
Jaime-GEB <jaimegomez@inade.es>
2026-02-05 16:42:53 +0100

    first commit dev

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
 const Home = lazy(() => import('./pages/Home'));
 
 const AppRouter = () => {
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={
+            <main className="h-full w-full flex justify-center items-center ">
+              <CircularProgress />
+            </main>
+        }>
             <Routes>
                 {/* Redirección condicional desde '/' */}
                 <Route path="/*" element={
